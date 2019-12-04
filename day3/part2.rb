7ba260e2da8992b3e13b91122690c90d5a6055bf
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2019-12-04 18:18:59 +0000

    Refactor day 3 / part 2 solution

--- day3/part2.rb
+++ day3/part2.rb
@@ -6,85 +6,76 @@ File.open('./input.txt', 'r') do |f|
   end
 end
 
-def move_wire(instruction, current_position, common_coords = [], acc_cost = 0)
+def process_instruction(instruction, current_position, known_intersections = [], acc_cost = 0)
   direction = instruction[0]
   value = instruction[1..-1].to_i
 
-  x = current_position[:x]
-  y = current_position[:y]
+  move_result = move_until_goal(
+    { x: current_position[:x], y: current_position[:y] },
+    direction,
+    value,
+    acc_cost,
+    known_intersections
+  )
 
-  path_positions = []
+  {
+    path_positions: move_result[:points_transversed],
+    intersections_with_cost: move_result[:intersections_acc_cost],
+    instruction_cost: value
+  }
+end
 
-  cost = 0
+def move_until_goal(start_coords, direction, value_to_move, accumulated_cost, known_intersections)
+  points_transversed = []
   intersections_with_cost = []
 
   case direction
   when 'L'
-    i = x
-    cost = value
-
-    goal_x = x - value
-    while i >= goal_x do
-      tmp_cost = acc_cost + (i - x).abs
-      new_tmp_position = { x: i, y: y }
-      path_positions << new_tmp_position
-      i -= 1
-
-      if common_coords.include? new_tmp_position
-        intersections_with_cost << { position: new_tmp_position, move_cost: tmp_cost }
-      end
-    end
+    changing_coord_symbol = :x
+    direction_multiplier = -1
   when 'U'
-    i = y
-    cost = value
-
-    goal_y = y + value
-    while i <= goal_y do
-      new_tmp_position = { x: x, y: i }
-      tmp_cost = acc_cost + (i - y).abs
-      path_positions << new_tmp_position
-      i += 1
-
-      if common_coords.include? new_tmp_position
-        intersections_with_cost << { position: new_tmp_position, move_cost: tmp_cost }
-      end
-    end
+    changing_coord_symbol = :y
+    direction_multiplier = 1
   when 'R'
-    i = x
-    cost = value
-
-    goal_x = x + value
-    while i <= goal_x do
-      tmp_cost = acc_cost + (i - x).abs
-      new_tmp_position = { x: i, y: y }
-      path_positions << new_tmp_position
-      i += 1
-
-      if common_coords.include? new_tmp_position
-        intersections_with_cost << { position: new_tmp_position, move_cost: tmp_cost }
-      end
-    end
+    changing_coord_symbol = :x
+    direction_multiplier = 1
   else
-    i = y
-    cost = value
+    changing_coord_symbol = :y
+    direction_multiplier = -1
+  end
 
-    goal_y = y - value
-    while i >= goal_y do
-      tmp_cost = acc_cost + (i - y).abs
-      new_tmp_position = { x: x, y: i }
-      path_positions << new_tmp_position
-      i -= 1
+  fixed_coord_symbol = changing_coord_symbol == :x ? :y : :x
 
-      if common_coords.include? new_tmp_position
-        intersections_with_cost << { position: new_tmp_position, move_cost: tmp_cost }
-      end
-    end
+  initial_value = start_coords[changing_coord_symbol]
+  goal_value = initial_value + (value_to_move * direction_multiplier)
+
+  current_position_value = initial_value
+  should_continue = true
+
+  while should_continue
+    should_continue = if direction_multiplier == -1
+                        current_position_value > goal_value
+                      else
+                        current_position_value < goal_value
+                      end
+
+    total_cost = accumulated_cost + (current_position_value - initial_value).abs
+
+    new_position = {}
+    new_position[changing_coord_symbol] = current_position_value
+    new_position[fixed_coord_symbol] = start_coords[fixed_coord_symbol]
+
+    points_transversed << new_position
+    current_position_value += (1 * direction_multiplier)
+
+    next unless known_intersections.include? new_position
+
+    intersections_with_cost << { position: new_position, move_cost: total_cost }
   end
 
   {
-    path_positions: path_positions,
-    intersections_with_cost: intersections_with_cost,
-    cost: cost
+    points_transversed: points_transversed,
+    intersections_acc_cost: intersections_with_cost
   }
 end
 
@@ -94,13 +85,16 @@ wire_paths.each do |paths|
   coordinates_for_path = [{ x: 0, y: 0 }]
 
   paths.split(',').each do |move|
-    coordinates_for_path.concat move_wire(move, coordinates_for_path.last)[:path_positions]
+    coordinates_for_path.concat(
+      process_instruction(move, coordinates_for_path.last)[:path_positions]
+    )
   end
 
   all_path_coordinates << coordinates_for_path
 end
 
 common_coordinates = all_path_coordinates.reduce(:&)
+
 all_intersections_coordinates = []
 
 wire_paths.each do |paths|
@@ -109,9 +103,9 @@ wire_paths.each do |paths|
   acc_cost = 0
 
   paths.split(',').each do |move|
-    move_data = move_wire(move, coordinates_for_path.last, common_coordinates, acc_cost)
+    move_data = process_instruction(move, coordinates_for_path.last, common_coordinates, acc_cost)
 
-    acc_cost += move_data[:cost]
+    acc_cost += move_data[:instruction_cost]
     coordinates_for_path.concat move_data[:path_positions]
 
     unless move_data[:intersections_with_cost].empty?
@@ -126,9 +120,9 @@ minimum_sum = 2**30 - 1
 
 all_intersections_coordinates.first.each do |first_intersection|
   all_intersections_coordinates.last.each do |second_intersection|
-    next if first_intersection[:position] != second_intersection[:position] ||
-            first_intersection[:position][:x] == 0 && first_intersection[:position][:y] == 0 ||
-            second_intersection[:position][:x] == 0 && second_intersection[:position][:y] == 0
+    next if first_intersection[:position] != second_intersection[:position]
+    next if first_intersection[:position][:x].zero? && first_intersection[:position][:y].zero?
+    next if second_intersection[:position][:x].zero? && second_intersection[:position][:y].zero?
 
     sum = first_intersection[:move_cost] + second_intersection[:move_cost]
     minimum_sum = sum if minimum_sum > sum
